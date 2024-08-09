3ab9d9c2c3ea752ea316abbdc5570b4d25d0cfb0
KaMiL374346363 <kamil.qehreman002@gmail.com>
2024-08-09 09:08:29 +0400

    My first react project

---
+++ 1)first react project/src/components/Cards.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Cards() {
+  return (
+    <div className="cards">
+      <div className="card">
+        <h2>Card 1</h2>
+        <p>This is the content of card 1.</p>
+      </div>
+      <div className="card">
+        <h2>Card 2</h2>
+        <p>This is the content of card 2.</p>
+      </div>
+      <div className="card">
+        <h2>Card 3</h2>
+        <p>This is the content of card 3.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
---
+++ 1)first react project/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Lorem ipsum dolor sit amet consectetur.</p>
+    </footer>
+  );
+}
+
+export default Footer;
